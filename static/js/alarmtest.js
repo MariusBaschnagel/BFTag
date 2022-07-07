d4599cf84f4c9cbb1a0ea24f66bd5ea31b3fd677
Marius Baschnagel <stmabasc@stud.h-da.de>
2022-07-07 23:14:01 +0200

    reformating alarmtest.js

--- static/js/alarmtest.js
+++ static/js/alarmtest.js
@@ -1,7 +1,8 @@
 let btn_probe = document.getElementById("probealarm");
+
+/* audio elements */
 const einsatz_alarm = new Audio("./static/audio/einsatz_alarmton.mp3");
 
-/* initialize alarmstichwort */
 const f1 = new Audio("./static/audio/stichwort/feuer_eins.mp3");
 const f2 = new Audio("./static/audio/stichwort/feuer_zwei.mp3");
 const f3 = new Audio("./static/audio/stichwort/feuer_drei.mp3");
@@ -9,13 +10,13 @@ const h1 = new Audio("./static/audio/stichwort/hilfe_eins.mp3");
 const h2 = new Audio("./static/audio/stichwort/hilfe_zwei.mp3");
 const h3 = new Audio("./static/audio/stichwort/hilfe_drei.mp3");
 
-/* initialize fahrzeuge audio */
 const HLF = new Audio("./static/audio/fahrzeuge/HLF.mp3");
 const TLF = new Audio("./static/audio/fahrzeuge/TLF.mp3");
 const LF8 = new Audio("./static/audio/fahrzeuge/LF8.mp3");
 const MTW = new Audio("./static/audio/fahrzeuge/MTW.mp3");
 const GWL = new Audio("./static/audio/fahrzeuge/GWL.mp3");
 const GWAS = new Audio("./static/audio/fahrzeuge/GWAS.mp3");
+/******************************************************************************/
 
 const audioArray = [einsatz_alarm]
 
