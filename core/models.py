b4e45f551ba561f5143d6bdb52493da1670b5b66
Marius Baschnagel <stmabasc@stud.h-da.de>
2024-03-03 22:48:33 +0100

    chore: models Fahrzeug and Logmessage added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Fahrzeug(models.Model):
+    name = models.CharField(max_length=50)
+    shortname = models.CharField(max_length=10)
+
+
+class Logmessage(models.Model):
+    stichwort = models.CharField(max_length=20)
+    adresse = models.TextField()
+    fahrzeuge = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
