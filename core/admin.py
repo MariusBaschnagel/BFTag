1cfe3ad4950e29711f6a0b3ce9fbd8ad609bc7d4
Marius Baschnagel <stmabasc@stud.h-da.de>
2024-03-03 23:16:48 +0100

    chore: add models to admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class FahrzeugModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'shortname')
+
+
+class LogMessageModelAdmin(admin.ModelAdmin):
+    list_display = ('stichwort', 'adresse', 'fahrzeuge', 'timestamp')
+    list_filter = ('timestamp', 'stichwort')
+
+
+admin.site.register(models.Fahrzeug, FahrzeugModelAdmin)
+admin.site.register(models.Logmessage, LogMessageModelAdmin)
